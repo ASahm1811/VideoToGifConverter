931f177deaf7ba4884f7373d4bf7117db98c7b21
Almir Šahman <a.sahman@live.nl>
2026-08-09 21:32:59 +0200

    Removed unused imports

--- VideoToGifConverter.Desktop/MainWindow.xaml.cs
+++ VideoToGifConverter.Desktop/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using Microsoft.Win32;
-using System.Diagnostics;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.IO;
 using VideoToGifConverter.Core.Services;
-using System.Threading.Tasks;
 
 namespace VideoToGifConverter.Desktop
 {
